eff0652983258d91978449d1eee3380818b5b5ef
Ebru Çakar <ebru_cakar_11@hotmail.com>
2021-09-25 12:34:38 +0300

    Sıfıra bölünme hatası giderildi.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -24,12 +24,13 @@ public class PlayerController : MonoBehaviour
         {
             return;
         }
-        if (Input.mousePosition.y > Screen.height / 3)
+
+        if (Input.mousePosition.y > Screen.height / 2)
         {
             lineRenderer.enabled = false;
             return;
         }
-        else if(isMouseButtonDown)
+        else if (isMouseButtonDown)
         {
             lineRenderer.enabled = true;
         }
@@ -57,6 +58,7 @@ public class PlayerController : MonoBehaviour
             if (Physics.Raycast(ray, out hit))
             {
                 lineRenderer.SetPosition(0, new Vector3(hit.point.x, 0, hit.point.z));
+                lineRenderer.SetPosition(1, new Vector3(hit.point.x, 0, hit.point.z));
             }
         }
         else if (Input.GetMouseButton(0) && countDown > maxCountDown)
@@ -67,12 +69,11 @@ public class PlayerController : MonoBehaviour
             }
             direction = new Vector3(endPosition.x, 0, endPosition.y) - new Vector3(startPosition.x, 0, startPosition.y);
             distance = direction.magnitude / 10;
-            //lineRenderer.SetPosition(1, direction.normalized * distance);
 
             endPosition = Input.mousePosition;
 
             ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            // Does the ray intersect any objects excluding the player layer
+
             if (Physics.Raycast(ray, out hit))
             {
                 lineRenderer.SetPosition(1, new Vector3(hit.point.x, 0, hit.point.z));
@@ -90,14 +91,16 @@ public class PlayerController : MonoBehaviour
                 endPosition = Input.mousePosition;
 
                 ray = Camera.main.ScreenPointToRay(startPosition);
-                // Does the ray intersect any objects excluding the player layer
+
                 if (Physics.Raycast(ray, out hit))
                 {
                     Vector3 newOne = lineRenderer.GetPosition(1) - lineRenderer.GetPosition(0);
-                    newOne = newOne / Vector3.Distance(lineRenderer.GetPosition(1), lineRenderer.GetPosition(0));
-                    newOne = lineRenderer.GetPosition(0) - (newOne * 8);
-
-                    GameManager.Instance.CreateWave(transform, new Vector3(newOne.x, .94f, newOne.z), -(Mathf.Atan2(lineRenderer.GetPosition(1).z - lineRenderer.GetPosition(0).z, lineRenderer.GetPosition(1).x - lineRenderer.GetPosition(0).x) * 180f / Mathf.PI), Vector3.one);
+                    if (Vector3.Distance(lineRenderer.GetPosition(1), lineRenderer.GetPosition(0)) != 0)
+                    {
+                        newOne = newOne / Vector3.Distance(lineRenderer.GetPosition(1), lineRenderer.GetPosition(0));
+                        newOne = lineRenderer.GetPosition(0) - (newOne * 8);
+                        GameManager.Instance.CreateWave(transform, new Vector3(newOne.x, .94f, newOne.z), -(Mathf.Atan2(lineRenderer.GetPosition(1).z - lineRenderer.GetPosition(0).z, lineRenderer.GetPosition(1).x - lineRenderer.GetPosition(0).x) * 180f / Mathf.PI), Vector3.one);
+                    }                    
                 }
                 lineRenderer.enabled = false;
                 isMouseButtonDown = false;
